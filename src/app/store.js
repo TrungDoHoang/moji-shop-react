4ac8300246f7889f37a72e8ba418ec09a9f397ad
trungdohoang <hoangtrung15012k@gmail.com>
2021-09-22 22:03:32 +0700

    fix newsSlice

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from './reducers/productsSlice'
 import cartReducer from './reducers/cartSlice'
 import categoryReducer from './reducers/categorySlice'
-import newsReducer from './reducers/NewsSlice'
+import newsReducer from './reducers/newsSlice'
 
 export const store = configureStore({
   reducer: {
