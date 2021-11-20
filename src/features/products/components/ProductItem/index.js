6cfbc44f535aeb8f1ac36ff87ba1419e4d4d04b7
trungdohoang <hoangtrung15012k@gmail.com>
2021-11-20 16:27:37 +0700

    fix image

--- src/features/products/components/ProductItem/index.js
+++ src/features/products/components/ProductItem/index.js
@@ -24,7 +24,7 @@ export default function ProductItem(props) {
             <div className="product">
                 <div className="product-img">
                     <Link to={`/detail/products?id=${props.id}`} className="text-decoration-none d-block" title={props.name}>
-                        <img src={`./assets/images/product/${props.img}`} alt={props.name}/>
+                        <img src={`/assets/images/product/${props.img}`} alt={props.name}/>
                     </Link>
                     <div className="product-action">
                         <span className="material-icons-outlined">
